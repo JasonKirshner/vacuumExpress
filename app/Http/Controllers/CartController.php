e730168298d2ce1c2a34a0220706ba1c6844a03a
Jason Kirshner <jasonkirshner13@gmail.com>
2019-07-18 23:39:21 -0400

    testing value
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -13,7 +13,7 @@ class CartController extends Controller
      */
     public function index()
     {
-        $cart = Cart::all()->toArray();
+        $cart = Cart::all();
         // return view('cart.index', compact('cart'));
         return $cart;
     }
