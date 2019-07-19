6cf975bc1c139b65d0e92c15656cf128bdb5e40b
Jason Kirshner <jasonkirshner13@gmail.com>
2019-07-19 16:50:41 -0400

    fixed variable syntax
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- resources/views/cart/destroy.blade.php
+++ resources/views/cart/destroy.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row">
 	<div class="col-md-12">
-		<p>{{deleted}}</p>
+		<p>{{$deleted}}</p>
 		<!-- @foreach($cart as $item)
 		<p>{{$item['Name']}}</p>
 		@endforeach -->
