a71834b1159efab93dc2a232ac499040f38f9fd8
Jason Kirshner <jasonkirshner13@gmail.com>
2019-07-31 22:53:44 -0400

    Defined authorization Gate
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-product', function ($user) {
+            return $user->role == 'admin';
+        });
     }
 }
