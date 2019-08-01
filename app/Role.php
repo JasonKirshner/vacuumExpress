877a9c9c3319bb79d2c464e7a80eb487d79d17c6
Jason Kirshner <jasonkirshner13@gmail.com>
2019-07-31 22:51:30 -0400

    Added Role model
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

---
+++ app/Role.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $created_at
+ * @property User[] $users
+ */
+class Role extends Model
+{
+
+    public $timestamps = true;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
