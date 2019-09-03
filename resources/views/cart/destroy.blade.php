f3a4c4cfe57a72da5801c83498e86368891882b2
Jason Kirshner <jasonkirshner13@gmail.com>
2019-09-03 12:49:39 -0400

    removed unecessary files
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- resources/views/cart/destroy.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-
-@section('content')
-<div class="row">
-	<div class="col-md-12">
-		<p>{{$deleted}}</p>
-	</div>
-</div>
-@endsection
--- resources/views/products/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-
-@section('content')
-<div class="row">
-	<div class="col-md-12">
-		<p>{{$products}}</p>
-	</div>
-</div>
-@endsection
